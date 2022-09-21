210f011d1e20d8dd916b63a3c302628993e223b5
Pedro Ceolato <pedroceolato2002@gmail.com>
2022-09-21 18:35:48 -0300

    finished web and refactor in server

--- web/src/components/Form/input.tsx
+++ web/src/components/Form/input.tsx
@@ -9,6 +9,7 @@ export function Input(props: InputProps) {
         <input
             {...props}
             className="bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc.500"
+            required
         />
     )
 }
