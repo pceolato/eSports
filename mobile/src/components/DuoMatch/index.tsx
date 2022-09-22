20968e9d4b069172274bba179c113d30fad30341
Pedro Ceolato <pedroceolato2002@gmail.com>
2022-09-22 18:45:59 -0300

    feat: completed mobile

--- mobile/src/components/DuoMatch/index.tsx
+++ mobile/src/components/DuoMatch/index.tsx
@@ -1,21 +1,77 @@
-import { View, Modal, ModalProps, Text } from 'react-native';
+import { View, Modal, ModalProps, Text, TouchableOpacity, Alert, ActivityIndicator } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons'
+import { CheckCircle } from 'phosphor-react-native';
+import * as Clipboard from 'expo-clipboard'
 
 import { styles } from './styles';
+import { THEME } from '../../theme';
+
+import { Heading } from '../../components/Heading';
+import { useState } from 'react';
 
 interface Props extends ModalProps {
-    discord: string;
+  discord: string;
+  onClose: () => void;
 }
 
-export function DuoMatch({discord, ...rest}: Props) {
+export function DuoMatch({ discord, onClose, ...rest }: Props) {
+  const [isCopping, setIsCopping] = useState(false);
+
+  async function handleCopyDiscordToClipBoard() {
+    setIsCopping(true);
+    await Clipboard.setStringAsync(discord);
+
+    Alert.alert('Discord copiado!', 'Usuário copiado, encontre seu duo no Discord!')
+    setIsCopping(false);
+  }
+
   return (
-    <Modal {...rest}>
-        <View style={styles.container}>
+    <Modal
+      animationType="fade"
+      transparent
+      statusBarTranslucent
+      {...rest}
+    >
+      <View style={styles.container}>
+        <View style={styles.content}>
+          <TouchableOpacity
+            style={styles.closeIcon}
+            onPress={onClose}
+          >
+            <MaterialIcons 
+              name="close"
+              size={20}
+              color={THEME.COLORS.CAPTION_500}
+            />
+          </TouchableOpacity>
 
+          <CheckCircle 
+            size={64}
+            color={THEME.COLORS.SUCCESS}
+            weight="bold"
+          />
+
+          <Heading 
+            title="Let's play!"
+            subtitle="Agora é só começar a jogar!"
+            style={{alignItems: 'center', marginTop: 24}}
+          />
+
+          <Text style={styles.label}>
+            Adicione no Discord
+          </Text>
+
+          <TouchableOpacity
+            style={styles.discordButton}
+            onPress={handleCopyDiscordToClipBoard}
+            disabled={isCopping}
+          >
             <Text style={styles.discord}>
-                {discord}
+              {isCopping ? <ActivityIndicator color={THEME.COLORS.PRIMARY}/> : discord}
             </Text>
-
+          </TouchableOpacity>
         </View>
+      </View>
     </Modal>
   );
 }
--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -5,9 +5,6 @@ import { FlatList, Image, TouchableOpacity, View, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { GameParams } from '../../@types/navigation';
 
-import { Background } from '../../components/Background';
-import { Heading } from '../../components/Heading';
-
 import logoImg from '../../assets/logo-nlw-esports.png'
 
 import { Entypo } from '@expo/vector-icons'
@@ -16,9 +13,13 @@ import { styles } from './styles';
 import { THEME } from '../../theme';
 
 import { DuoCard, DuoCardProps } from '../../components/DuoCard';
+import { Background } from '../../components/Background';
+import { Heading } from '../../components/Heading';
+import { DuoMatch } from '../../components/DuoMatch';
 
 export function Game() {
     const [duos, setDuos] = useState<DuoCardProps[]>([]);
+    const [discordDuoSelected, setDiscordDuoSelected] = useState('');
 
     const navigation = useNavigation();
     const route = useRoute();
@@ -28,66 +29,80 @@ export function Game() {
         navigation.goBack();
     }
 
+    async function getDiscordUser(adsId: string) {
+        fetch(`http://192.168.0.34:3333/ads/${adsId}/discord`)
+            .then(response => response.json())
+            .then(data => {
+                setDiscordDuoSelected(data.discord)
+            }
+        )
+    }
+
     useEffect(() => {
         fetch(`http://192.168.0.34:3333/games/${game.id}/ads`)
-          .then(response => response.json())
-          .then(data => {
-            setDuos(data)
-          }
-          )
-      },[])
+            .then(response => response.json())
+            .then(data => {
+                setDuos(data)
+            }
+            )
+    }, [])
 
     return (
         <Background>
             <SafeAreaView style={styles.container}>
-            <View style={styles.header}>
-                <TouchableOpacity onPress={handleGoBack}>
-                    <Entypo 
-                        name="chevron-thin-left"
-                        color={THEME.COLORS.CAPTION_300}
-                        size={20}
+                <View style={styles.header}>
+                    <TouchableOpacity onPress={handleGoBack}>
+                        <Entypo
+                            name="chevron-thin-left"
+                            color={THEME.COLORS.CAPTION_300}
+                            size={20}
+                        />
+                    </TouchableOpacity>
+
+                    <Image
+                        source={logoImg}
+                        style={styles.logo}
                     />
-                </TouchableOpacity>
+
+                    <View style={styles.right} />
+                </View>
 
                 <Image
-                    source={logoImg}
-                    style={styles.logo}
+                    source={{ uri: game.bannerUrl }}
+                    style={styles.cover}
+                    resizeMode="cover"
                 />
 
-                <View style={styles.right}/>
-            </View>
-            
-            <Image 
-                source={{uri: game.bannerUrl}}
-                style={styles.cover}
-                resizeMode="cover"
-            />
+                <Heading
+                    title={game.title}
+                    subtitle="Conecte-se e comece a jogar"
+                />
 
-            <Heading 
-                title={game.title}
-                subtitle="Conecte-se e comece a jogar"
-            />
+                <FlatList
+                    data={duos}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => (
+                        <DuoCard
+                            data={item}
+                            onConnect={() => getDiscordUser(item.id)}
+                        />
+                    )}
+                    horizontal
+                    style={styles.containerList}
+                    contentContainerStyle={[duos.length > 0 ? styles.contentList : styles.emptyListContent]}
+                    showsHorizontalScrollIndicator={false}
+                    ListEmptyComponent={() => (
+                        <Text style={styles.emptyListText}>
+                            Não há anúncios publicados ainda.
+                        </Text>
+                    )}
+                />
 
-            <FlatList 
-                data={duos}
-                keyExtractor={item => item.id}
-                renderItem={({item}) => (
-                    <DuoCard 
-                        data={item} 
-                        onConnect={() => {}}
-                    />
-                )}
-                horizontal
-                style={styles.containerList}
-                contentContainerStyle={[duos.length > 0 ? styles.contentList : styles.emptyListContent]}
-                showsHorizontalScrollIndicator={false}
-                ListEmptyComponent={() => (
-                    <Text style={styles.emptyListText}>
-                        Não há anúncios publicados ainda.
-                    </Text>
-                )}
-            />
-
+                <DuoMatch
+                    visible={discordDuoSelected.length > 0}
+                    discord={discordDuoSelected}
+                    onClose={() => setDiscordDuoSelected('')}
+                />
             </SafeAreaView>
         </Background>
     );
